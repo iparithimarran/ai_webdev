6dfad7b4e68b6cff3ba580c2f9e3b132d6bd81a1
iparithimarran <iparithimarran2006@gmail.com>
2026-04-05 00:54:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,4 +122,6 @@ if __name__ == "__main__":
     print("  Predict      : POST /predict")
     print("=" * 50 + "\n")
 
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
